9199c9a721d5208c2b8a14f29c99d3a473e3bdd5
Keith Bloom <keith.bloom@gmail.com>
2020-12-17 16:01:50 +0000

    adds POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Splitter } from './Splitter';
+import "./App.css";
+
+type BoundingBox = {
+  left: number;
+  right: number;
+  top: number;
+  bottom: number;
+  height: number;
+  width: number;
+};
+
+type HorizontalBoundingBox = Omit<BoundingBox, "left" | "right">;
+
+enum ElementType {
+  Top,
+  Bottom,
+  Splitter,
+}
+
+const getPosition = (
+  containerBox: HorizontalBoundingBox,
+  elementType: ElementType,
+  midPoint?: number
+): HorizontalBoundingBox => {
+  const splitterHeight = 5;
+  const { top, bottom, height, width } = containerBox;
+  const topPanelHeight = (midPoint ? midPoint : height / 2);
+
+  switch (elementType) {
+    case ElementType.Top: {
+      return {
+        top,
+        width,
+        bottom: topPanelHeight,
+        height: topPanelHeight,
+      };
+    }
+    case ElementType.Bottom: {
+      return {
+        top: topPanelHeight + splitterHeight,
+        width,
+        bottom,
+        height: height - topPanelHeight,
+      };
+    }
+    case ElementType.Splitter: {
+      return {
+        top: topPanelHeight,
+        width,
+        bottom: topPanelHeight + splitterHeight,
+        height: splitterHeight,
+      };
+    }
+  }
+};
+
+const getStyle = ({ top, bottom, height, width }: HorizontalBoundingBox) => {
+  return {
+    top: `${top}px`,
+    bottom: `${bottom}px`,
+    height: `${height}px`,
+    width: `${width}px`,
+  };
+};
+
+const updateEl = (el: HTMLDivElement, boundingBox: HorizontalBoundingBox) => {
+  const styles = getStyle(boundingBox);
+  el.style.height = styles.height;
+  el.style.width = styles.width;
+  el.style.top = styles.top;
+  el.style.bottom = styles.bottom;
+}
+
 
 function App() {
+  const continerRef = useRef<HTMLDivElement>(null);
+  const topRef = useRef<HTMLDivElement>(null);
+  const splitterRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+  const [dragPosition, setDragePosition] = useState<number | undefined>(undefined);
+
+  const [containerSize, setContainerSize] = useState<
+    HorizontalBoundingBox | undefined
+  >();
+
+  useLayoutEffect(() => {
+    if (continerRef.current) {
+      setContainerSize(continerRef.current.getBoundingClientRect());
+    }
+    console.log(continerRef.current?.getBoundingClientRect());
+  }, []);
+
+  useEffect(() => {
+    if (containerSize) {
+      console.log(getPosition(containerSize, ElementType.Top));
+    }
+  }, [containerSize]);
+
+  useEffect(() => {
+    if(topRef.current && splitterRef.current && bottomRef.current && containerSize) {
+      updateEl(topRef.current, getPosition(containerSize, ElementType.Top, dragPosition));
+      updateEl(splitterRef.current, getPosition(containerSize, ElementType.Splitter, dragPosition));
+      updateEl(bottomRef.current, getPosition(containerSize, ElementType.Bottom, dragPosition));
+    }
+
+  },[dragPosition, containerSize])
+
+  const handleOnChange = (position: number) => setDragePosition(position);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={continerRef} className="container">
+      {containerSize && (
+        <>
+          <div
+            ref={topRef}
+            className="top"
+            style={getStyle(getPosition(containerSize, ElementType.Top))}
+          >
+            Top panel
+          </div>
+          <Splitter
+            ref={splitterRef}
+            style={getStyle(getPosition(containerSize, ElementType.Splitter))}
+            onChange={handleOnChange}
+          />
+          <div
+            ref={bottomRef}
+            className="bottom"
+            style={getStyle(getPosition(containerSize, ElementType.Bottom))}
+          >
+            Bottom panel
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Splitter.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+
+export type SplitterProps = {
+  style: React.CSSProperties;
+  onChange: (position: number) => void;
+};
+
+export const Splitter = React.forwardRef<HTMLDivElement, SplitterProps>(({ style, onChange }, ref: any) => {
+
+  const splitterYPosition = useRef<number | null>(null);
+
+  const onMouseMove = (e: MouseEvent) => {
+    if (splitterYPosition.current) {
+      splitterYPosition.current = e.clientY;
+      onChange(splitterYPosition.current);
+    }
+  };
+
+  const onMouseDown = (e: React.MouseEvent) =>
+    (splitterYPosition.current = e.clientY);
+
+  const onMouseUp = () => {
+    splitterYPosition.current = null;
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  });
+
+  return (
+    <div
+      ref={ref}
+      className="splitter"
+      onMouseDown={onMouseDown}
+      style={style}
+    ></div>
+  );
+});
